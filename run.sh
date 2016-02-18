5184ce4f9c485a0d8854bad5c7cf663be0b19592
Michael Amireh <mma38e@gmail.com>
2016-02-18 15:58:23 -0800

    This should create the app, 2 forms and 2 databases

--- run.sh
+++ run.sh
@@ -1,6 +1,20 @@
+#creates basic app
+
 mkdir ARMY
 cd ARMY
 rails new team
 cd team
-cd bin
-rails server
+bin/rails server
+
+#create Student form and table(scaffold)
+
+bin/rails generate scaffold Student firstName:string lastName:string email:string studentId:string
+
+#migrate scaffold into database
+bin/rake db:migrate
+
+#Create events
+bin/rails generate scaffold events eventName:string eventLocation:string eventDate:date eventTime:time eventInfo:text
+
+#migrate scaffold into database
+bin/rake db:migrate
